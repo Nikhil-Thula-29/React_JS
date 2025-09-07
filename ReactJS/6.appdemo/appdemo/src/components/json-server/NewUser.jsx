c1ef360b2f1f6b89688748bc324bb10b695d9b31
Nikhil-Thula-29 <nikhil.thula29@gmail.com>
2025-09-08 01:28:46 +0530

    post

--- ReactJS/6.appdemo/appdemo/src/components/json-server/NewUser.jsx
+++ ReactJS/6.appdemo/appdemo/src/components/json-server/NewUser.jsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
 import "./NewUser.css";
+import axios from "axios";
+
 
 function NewUser(){
+
+    let[newuser,setNewUser]=useState({
+        name:"",
+        city:""
+    });
+
+
+
+    const getNewUser=(e)=>{
+         e.preventDefault();
+        axios.post('http://localhost:3000/result',newuser)
+        .then((res)=>{
+            console.log(res.data);
+            location.reload("/jsonserver");
+        })
+        .catch((err)=>{
+            console.log(err);
+            alert("Something went wrong");
+        });
+    }
+
     return (
         <div className="new-user">
             <h2>New Users</h2>
-            <input/>
+            <form onSubmit={getNewUser}>
+            <input type="text" placeholder="username" required
+            onChange={(eve)=>{
+                setNewUser({...newuser,name:eve.target.value});
+            }}/>
             <br></br>
             <br></br>
-            <select>
-                <option>Select City</option>
+            <select required onChange={(event)=>{
+                setNewUser({...newuser,city:event.target.value});
+            }}>
+                <option value="">Select City</option>
                 <option>City1</option>
                 <option>City2</option>
                 <option>City3</option>
@@ -17,7 +47,8 @@ function NewUser(){
             </select>
             <br></br>
             <br></br>
-            <button>New User</button>
+            <button type="submit">New User</button>
+            </form>
         </div>
     );
 }
